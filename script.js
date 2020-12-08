19d8d126fdf2a5aea60f91b47caf202ae64b9bdc
Ishaan-11 <ishaansharma1998@gmail.com>
2020-12-08 20:22:35 +0530

    add script file

---
+++ script.js
@@ -0,0 +1,74 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector('small');
+  small.innerHTML = message;
+}
+
+//show input success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+//check email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `${getFieldName(input)} is not valid`)
+  }
+  return re.test(String(email).toLowerCase());
+}
+
+//check required fields
+function requiredCheck(inputArr) {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+//check length
+function lengthCheck(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+//check password match
+function checkPasswordMatch(input1, input2) {
+  if(input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
+//get field name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  requiredCheck([username, email, password, password2]);
+  lengthCheck(username, 3, 15);
+  lengthCheck(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, password2);
+})
